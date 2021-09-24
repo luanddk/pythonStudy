dfdbde3453530b6dc9681e815551aaa10be783f9
luanddk <luandk2@hotmail.com>
2021-09-23 23:11:19 -0300

    Estudando Python

---
+++ aula11/aula11.py
@@ -0,0 +1,17 @@
+"""
+Operadores lógicos
+
+and, or, not
+in e not in
+"""
+
+usuario = input('Nome de usuário: ')
+senha = input('Senha do usuário: ')
+
+usuario_bd = 'luan'
+senha_bd = '123456'
+
+if usuario_bd == usuario and senha_bd == senha:
+    print('Você está logado no sistema.')
+else:
+    print('Usuário ou senha inválidos.')
